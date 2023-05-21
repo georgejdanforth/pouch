3c5edf89ba2f596fdb7080e12c64957bb073d469
George Danforth <georgejdanforth@gmail.com>
2023-05-21 17:36:13 -0400

    Add command line arg parsing

--- build.sbt
+++ build.sbt
@@ -27,6 +27,7 @@ version := "1.0"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.16" % Test
+libraryDependencies += "com.github.scopt" %% "scopt" % "4.1.0"
 
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the scala-parser-combinators dependency to the set of dependencies
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,35 @@
 package pouch
 
 import scala.io.StdIn
+import scala.sys
+
+import scopt.OParser
 
 object Main {
-  def main(args: Array[String]): Unit = {
+
+  object RunMode {
+    final val Init = "init"
+    final val CLI = "cli"
+  }
+
+  case class Config(
+    mode: String = "",
+  )
+
+  val builder = OParser.builder[Config]
+  val argParser = {
+    import builder._
+    OParser.sequence(
+      programName("pouch"),
+      head("pouch", "0.0.0"),
+      cmd(RunMode.Init)
+        .action((_, c) => c.copy(mode = RunMode.Init)),
+      cmd(RunMode.CLI)
+        .action((_, c) => c.copy(mode = RunMode.CLI)),
+    )
+  }
+
+  private def cli(): Unit = {
     while (true) {
       print("> ")
       val input = scala.io.StdIn.readLine()
@@ -15,6 +41,19 @@ object Main {
         case cmd : DelCommand => println("DEL %s".format(cmd.key))
         case cmd : ErrCommand => println("Error: %s".format(cmd.message))
       }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    OParser.parse(argParser, args, Config()) match {
+      case Some(config) => {
+        config.mode match {
+          case RunMode.Init => println("init")
+          case RunMode.CLI => cli()
+          case _ => println(OParser.usage(argParser))
+        }
+      }
+      case _ => sys.exit(1)
     }
   }
 }
